110ab2e294be4d54f55c72647c91284b6b918d1c
Vincent <hanyipeng1024@gmail.com>
2020-10-08 18:29:57 +1100

    fixed some bugs

--- FilmFinder/FilmFinder.sql
+++ FilmFinder/FilmFinder.sql
@@ -16,7 +16,7 @@ CREATE TABLE "admin" (
 
 -- INSERT DEFAULT ADMIN
 -- user name: admin password:admin
-INSERT INTO "admin" VALUES (1, 'admin', 'admin', '2020-10-08 09:14:23');
+INSERT INTO "admin" VALUES (1, "admin", "admin", "2020-10-08 09:14:2");
 
 -- Table structure for adminlog
 DROP TABLE IF EXISTS "adminlog";
@@ -26,23 +26,23 @@ CREATE TABLE "adminlog" (
     "ip" VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
     "login_time" datetime(0) DEFAULT NULL,
     PRIMARY KEY("id") USING BTREE,
-    INDEX 'admin_id' ('admin_id') USING BTREE,
-    INDEX 'ix_admin_create_time'('login_time') USING BTREE,
-    CONSTRAINT 'adminlog_ibfk_1' FOREIGN KEY('admin_id') REFERENCES 'admin'('id') ON DELETE RESTRICT ON UPDATE RESTRICT 
+    INDEX "admin_id" ("admin_id") USING BTREE,
+    INDEX "ix_admin_create_time"("login_time") USING BTREE,
+    CONSTRAINT "adminlog_ibfk_1" FOREIGN KEY("admin_id") REFERENCES "admin"("id") ON DELETE RESTRICT ON UPDATE RESTRICT 
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 
 -- Table structure about cast
-DROP TABLE IF EXISTS 'cast';
-CREATE TABLE 'cast'(
-    'id' int(11) NOT NULL AUTO_INCREMENT,
-    'first_name' VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    'second_name' VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    'name' VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    'email' VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    'phone' VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    'face' VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    PRIMARY KEY('id') USING BTREE
+DROP TABLE IF EXISTS "cast";
+CREATE TABLE "cast"(
+    "id" int(11) NOT NULL AUTO_INCREMENT,
+    "first_name" VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    "second_name" VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    "name" VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    "email" VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    "phone" VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    "face" VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY("id") USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 
