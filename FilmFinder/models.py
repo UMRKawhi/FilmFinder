0ef093b7d82c401674a7c5b7033d413600b2af86
lks352786399 <49390824+lks352786399@users.noreply.github.com>
2020-10-10 21:14:50 +1100

    Comment the blacklist & adminlog
    Two section will apply in the future
    
    Co-Authored-By: MingyanYuu <71819498+MingyanYuu@users.noreply.github.com>
    Co-Authored-By: ZiweiLi9908 <71751001+ZiweiLi9908@users.noreply.github.com>

--- FilmFinder/models.py
+++ FilmFinder/models.py
@@ -246,23 +246,23 @@ class GenreTag(db.Model):
     def __repr__(self):
         return "<GenreTag %r>" % self.id
 
-class BlackList(db.Model):
-    __tablename__ = 'blacklist'
-    # id
-    id = db.Column(db.Integer, primary_key=True)
-    # user id
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # black id
-    black_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # create time
-    create_time = db.Column(db.DateTime, index=True, default=datetime.datetime.now)
-
-    # format output
-    def __str__(self):
-        return "<BlackList %r>" % self.id
-
-    def __repr__(self):
-        return "<BlackList %r>" % self.id
+# class BlackList(db.Model):
+#     __tablename__ = 'blacklist'
+#     # id
+#     id = db.Column(db.Integer, primary_key=True)
+#     # user id
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     # black id
+#     black_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     # create time
+#     create_time = db.Column(db.DateTime, index=True, default=datetime.datetime.now)
+#
+#     # format output
+#     def __str__(self):
+#         return "<BlackList %r>" % self.id
+#
+#     def __repr__(self):
+#         return "<BlackList %r>" % self.id
 
 class WishList(db.Model):
     __tablename__ = 'wishlist'
@@ -343,22 +343,22 @@ class Admin(db.Model):
     def __repr__(self):
         return "<Admin %r>" % self.name
 
-class AdminLog(db.Model):
-    __tablename__ = "adminlog"
-    # id
-    id = db.Column(db.Integer, primary_key=True)
-    # admin id
-    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
-    # ip address
-    ip = db.Column(db.String(100))
-    # login time
-    login_time = db.Column(db.DateTime, index=True, default=datetime.datetime.now)
-
-    def __str__(self):
-        return "<Adminlog %r>" % self.id
-
-    def __repr__(self):
-        return "<Adminlog %r>" % self.id
+# class AdminLog(db.Model):
+#     __tablename__ = "adminlog"
+#     # id
+#     id = db.Column(db.Integer, primary_key=True)
+#     # admin id
+#     admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+#     # ip address
+#     ip = db.Column(db.String(100))
+#     # login time
+#     login_time = db.Column(db.DateTime, index=True, default=datetime.datetime.now)
+#
+#     def __str__(self):
+#         return "<Adminlog %r>" % self.id
+#
+#     def __repr__(self):
+#         return "<Adminlog %r>" % self.id
 
 class OperateLog(db.Model):
     __tablename__ = "operatelog"
