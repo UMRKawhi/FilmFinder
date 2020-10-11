6c68c4402e53259142b117e13c7c78d7edbf1af3
Vincent <hanyipeng1024@gmail.com>
2020-10-11 17:53:12 +1100

    Update the sql script

--- FilmFinder/FilmFinder.sql
+++ FilmFinder/FilmFinder.sql
@@ -1,8 +1,9 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-
--- TABLE ABOUT ADMIN
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -14,12 +15,14 @@ CREATE TABLE `admin`  (
   INDEX `ix_admin_create_time`(`create_time`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'admin', 'admin', '2020-10-11 10:23:41');
 
--- INSERT RECORD ABOUT ADMIN
-INSERT INTO `admin` VALUES (1, 'admin', 'admin', '2020-10-06 10:23:41');
-
-
--- TABLE ABOUT ADMINLOG
+-- ----------------------------
+-- Table structure for adminlog
+-- ----------------------------
 DROP TABLE IF EXISTS `adminlog`;
 CREATE TABLE `adminlog`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,8 +35,22 @@ CREATE TABLE `adminlog`  (
   CONSTRAINT `adminlog_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for blacklist
+-- ----------------------------
+DROP TABLE IF EXISTS `blacklist`;
+CREATE TABLE `blacklist`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `create_time` datetime(0) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  INDEX `ix_blacklist_create_time`(`create_time`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- TABLE ABOUT CAST
+-- ----------------------------
+-- Table structure for cast
+-- ----------------------------
 DROP TABLE IF EXISTS `cast`;
 CREATE TABLE `cast`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -46,8 +63,9 @@ CREATE TABLE `cast`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-
--- TABLE ABOUT MOVIE COMMENT
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,13 +82,16 @@ CREATE TABLE `comment`  (
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, '123', 3, 1, '2020-10-11 16:00:17', 0);
+INSERT INTO `comment` VALUES (2, '456', 3, 1, '2020-10-11 11:56:46', 5);
+INSERT INTO `comment` VALUES (3, '789', 3, 1, '2020-10-11 12:44:34', 5);
 
--- INSERT SOME COMMENT TODO
--- 
--- 
-
-
--- TABLE ABOUT DIRECT
+-- ----------------------------
+-- Table structure for direct
+-- ----------------------------
 DROP TABLE IF EXISTS `direct`;
 CREATE TABLE `direct`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -85,14 +106,15 @@ CREATE TABLE `direct`  (
   CONSTRAINT `direct_ibfk_2` FOREIGN KEY (`director_id`) REFERENCES `director` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-
--- INSERT SOME DIRECT
 -- ----------------------------
-INSERT INTO `direct` VALUES (1, NULL, 1, '2020-10-06 14:26:16');
-INSERT INTO `direct` VALUES (2, 3, 1, '2020-10-06 14:35:36');
+-- Records of direct
+-- ----------------------------
+INSERT INTO `direct` VALUES (1, NULL, 1, '2020-10-11 14:26:16');
+INSERT INTO `direct` VALUES (2, 3, 1, '2020-10-11 14:35:36');
 
-
--- TABLE ABOUT DIRECTOR
+-- ----------------------------
+-- Table structure for director
+-- ----------------------------
 DROP TABLE IF EXISTS `director`;
 CREATE TABLE `director`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -106,28 +128,18 @@ CREATE TABLE `director`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of director
+-- ----------------------------
+INSERT INTO `director` VALUES (1, NULL, NULL, NULL, NULL, NULL, 'Steven Spielberg', '2020-10-11 11:22:38');
+INSERT INTO `director` VALUES (2, NULL, NULL, NULL, NULL, NULL, 'Martin Scorsese', '2020-10-11 11:22:41');
+INSERT INTO `director` VALUES (3, NULL, NULL, NULL, NULL, NULL, 'Quentin Tarantino', '2020-10-11 11:22:43');
+INSERT INTO `director` VALUES (4, NULL, NULL, NULL, NULL, NULL, 'Ridley Scott', '2020-10-11 11:22:45');
+INSERT INTO `director` VALUES (5, NULL, NULL, NULL, NULL, NULL, 'James Cameron', '2020-10-11 11:22:48');
 
--- INSERT SOME DIRECTS
-INSERT INTO `director` VALUES (1, NULL, NULL, NULL, NULL, NULL, 'Steven Spielberg', '2020-10-06 11:22:38');
-INSERT INTO `director` VALUES (2, NULL, NULL, NULL, NULL, NULL, 'Martin Scorsese', '2020-10-06 11:22:41');
-INSERT INTO `director` VALUES (3, NULL, NULL, NULL, NULL, NULL, 'Quentin Tarantino', '2020-10-06 11:22:43');
-INSERT INTO `director` VALUES (4, NULL, NULL, NULL, NULL, NULL, 'Ridley Scott', '2020-10-06 11:22:45');
-INSERT INTO `director` VALUES (5, NULL, NULL, NULL, NULL, NULL, 'James Cameron', '2020-10-06 11:22:48');
-
-
--- TABLE ABOUT BLACKLIST
-DROP TABLE IF EXISTS `blacklist`;
-CREATE TABLE `blacklist`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `create_time` datetime(0) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `user_id`(`user_id`) USING BTREE,
-  INDEX `ix_blacklist_create_time`(`create_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
-
--- TABLE ABOUT FILMS
+-- ----------------------------
+-- Table structure for film
+-- ----------------------------
 DROP TABLE IF EXISTS `film`;
 CREATE TABLE `film`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -146,12 +158,14 @@ CREATE TABLE `film`  (
   INDEX `ix_film_create_time`(`create_time`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of film
+-- ----------------------------
+INSERT INTO `film` VALUES (3, 'The Lost World: Jurassic Park', 'The Lost World: Jurassic Park is a 1997 American science fiction adventure film and the second installment in the Jurassic Park film series. A sequel to 1993\'s Jurassic Park and loosely based on Michael Crichton\'s 1995 novel The Lost World, the film was directed by Steven Spielberg and written by David Koepp. The film stars Jeff Goldblum, returning as the eccentric chaos theorist and mathematician Ian Malcolm, as well as Julianne Moore, Pete Postlethwaite, Arliss Howard, Vince Vaughn, and Vanessa Lee Chester. Four years after the disaster at Jurassic Park, John Hammond (Richard Attenborough) sends a team, led by Malcolm, to Isla Sorna, the second island Hammond\'s company InGen used to make the dinosaurs, to study the animals while coming into conflict with a team led by InGen to bring some of the dinosaurs back to the United States.\r\n\r\nAfter the original novel\'s release and the first film\'s success, fans pressured Crichton for a sequel. Following the book\'s publication in 1995, production began on a film sequel. Filming took place from September to December 1996, primarily in California, with a shoot in Kauai, Hawaii, where the first film was shot. The Lost World\'s plot and imagery is substantially darker than Jurassic Park. It makes more extensive use of computer-generated imagery to depict the dinosaurs, along with life-sized animatronics.\r\n\r\nReleased on May 23, 1997, the film received mixed reviews from critics, who praised the visuals and action sequences, but criticized the writing, and character development. The film was a box office success, grossing over $618 million worldwide, becoming the second-highest-grossing film of 1997. It earned an Academy Award nomination for Best Visual Effects. A sequel, Jurassic Park III, was released on July 18, 2001.', '20201006143536afefcd5eb58043d2adc9b97c18596787.jpg', 3, 107, 3, '2020-10-25', '128', '2020-10-11 14:35:36');
 
--- INSERT FILM
-INSERT INTO `film` VALUES (3, 'The Lost World: Jurassic Park', 'The Lost World: Jurassic Park is a 1997 American science fiction adventure film and the second installment in the Jurassic Park film series. A sequel to 1993\'s Jurassic Park and loosely based on Michael Crichton\'s 1995 novel The Lost World, the film was directed by Steven Spielberg and written by David Koepp. The film stars Jeff Goldblum, returning as the eccentric chaos theorist and mathematician Ian Malcolm, as well as Julianne Moore, Pete Postlethwaite, Arliss Howard, Vince Vaughn, and Vanessa Lee Chester. Four years after the disaster at Jurassic Park, John Hammond (Richard Attenborough) sends a team, led by Malcolm, to Isla Sorna, the second island Hammond\'s company InGen used to make the dinosaurs, to study the animals while coming into conflict with a team led by InGen to bring some of the dinosaurs back to the United States.\r\n\r\nAfter the original novel\'s release and the first film\'s success, fans pressured Crichton for a sequel. Following the book\'s publication in 1995, production began on a film sequel. Filming took place from September to December 1996, primarily in California, with a shoot in Kauai, Hawaii, where the first film was shot. The Lost World\'s plot and imagery is substantially darker than Jurassic Park. It makes more extensive use of computer-generated imagery to depict the dinosaurs, along with life-sized animatronics.\r\n\r\nReleased on May 23, 1997, the film received mixed reviews from critics, who praised the visuals and action sequences, but criticized the writing, and character development. The film was a box office success, grossing over $618 million worldwide, becoming the second-highest-grossing film of 1997. It earned an Academy Award nomination for Best Visual Effects. A sequel, Jurassic Park III, was released on July 18, 2001.', '20201006143536afefcd5eb58043d2adc9b97c18596787.jpg', 3, 107, 3, '2020-10-25', '128', '2020-10-06 14:35:36');
-
-
--- TABLE ABOUT GENRE
+-- ----------------------------
+-- Table structure for genre
+-- ----------------------------
 DROP TABLE IF EXISTS `genre`;
 CREATE TABLE `genre`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -162,15 +176,74 @@ CREATE TABLE `genre`  (
   INDEX `ix_genre_create_time`(`create_time`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of genre
+-- ----------------------------
+INSERT INTO `genre` VALUES (1, 'science', '2020-10-11 10:52:14');
+INSERT INTO `genre` VALUES (2, 'horror', '2020-10-11 10:56:29');
+INSERT INTO `genre` VALUES (3, 'action', '2020-10-11 10:56:33');
+INSERT INTO `genre` VALUES (5, 'animation', '2020-10-11 10:56:46');
+INSERT INTO `genre` VALUES (6, 'music', '2020-10-11 10:56:54');
 
+-- ----------------------------
+-- Table structure for genretag
+-- ----------------------------
+DROP TABLE IF EXISTS `genretag`;
+CREATE TABLE `genretag`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `film_id` int(11) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL,
+  `create_time` datetime(0) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `film_id`(`film_id`) USING BTREE,
+  INDEX `genre_id`(`genre_id`) USING BTREE,
+  INDEX `ix_genretag_create_time`(`create_time`) USING BTREE,
+  CONSTRAINT `genretag_ibfk_1` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `genretag_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- INSERT SOME GENRES
--- 
--- 
+-- ----------------------------
+-- Records of genretag
+-- ----------------------------
+INSERT INTO `genretag` VALUES (1, NULL, 1, '2020-10-11 14:26:16');
+INSERT INTO `genretag` VALUES (2, 3, 1, '2020-10-11 14:35:36');
 
+-- ----------------------------
+-- Table structure for operatelog
+-- ----------------------------
+DROP TABLE IF EXISTS `operatelog`;
+CREATE TABLE `operatelog`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) DEFAULT NULL,
+  `ip` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `reason` varchar(600) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `create_time` datetime(0) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `admin_id`(`admin_id`) USING BTREE,
+  INDEX `ix_operatelog_create_time`(`create_time`) USING BTREE,
+  CONSTRAINT `operatelog_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for performance
+-- ----------------------------
+DROP TABLE IF EXISTS `performance`;
+CREATE TABLE `performance`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `film_id` int(11) DEFAULT NULL,
+  `cast_id` int(11) DEFAULT NULL,
+  `create_time` datetime(0) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `film_id`(`film_id`) USING BTREE,
+  INDEX `cast_id`(`cast_id`) USING BTREE,
+  INDEX `ix_performance_create_time`(`create_time`) USING BTREE,
+  CONSTRAINT `performance_ibfk_1` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `performance_ibfk_2` FOREIGN KEY (`cast_id`) REFERENCES `cast` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for user
-
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -188,3 +261,5 @@ CREATE TABLE `user`  (
   UNIQUE INDEX `phone`(`phone`) USING BTREE,
   UNIQUE INDEX `face`(`face`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+
