edafcb90fde8d8fbe4e23e119b083792335ffc30
lks352786399 <49390824+lks352786399@users.noreply.github.com>
2020-10-11 12:16:00 +1100

    Update __init__.py
    Co-Authored-By: MingyanYuu <71819498+MingyanYuu@users.noreply.github.com>

--- FilmFinder/__init__.py
+++ FilmFinder/__init__.py
@@ -1,42 +1,34 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from .admin import admin as admin_blueprint
 from .home import home as home_blueprint
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "ysql+pymysql://root:root@127.0.0.1:3306/film"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+    app.config['SECRET_KEY'] = '1234567890'
+    app.config['UP_DIR'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/media/')
+    app.config['USER_IMAGE'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/user/')
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(home_blueprint)
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    app.register_blueprint(admin_blueprint)
-    app.register_blueprint(home_blueprint)
-
     return app
-
 
 app = create_app()
 db = SQLAlchemy(app)
-
-
-# 404 page
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
-
-# 401 page
-@app.errorhandler(401)
-def unauthorized_access(error):
-    return render_template('401.html'), 401
