037dd11bc9dde95a8aee5e01214028544e3ef351
ZiweiLi9908 <71751001+ZiweiLi9908@users.noreply.github.com>
2020-10-13 21:58:22 +1100

    change the secret key
    change the original secret key with more random one. improve the safety for the system
    
    Co-Authored-By: UMRKawhi <48422665+UMRKawhi@users.noreply.github.com>

--- FilmFinder/config.py
+++ FilmFinder/config.py
@@ -1,7 +1,10 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/film'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = '1234567890'
-UP_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/media/')
-USER_IMAGE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/user/')
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@127.0.0.1:3306/film'
+SQLALCHEMY_TRACK_MODIFICATIONS=True
+SECRET_KEY='b1b7ed6af47d4031acbdeb420658ba84'
+UP_DIR=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/media/')
+USER_IMAGE=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/user/')
+
+
+
