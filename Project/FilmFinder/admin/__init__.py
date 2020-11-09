adae0a04f79eb7cc1a378ed2ebdb69db42839236
lks352786399 <49390824+lks352786399@users.noreply.github.com>
2020-11-09 21:16:51 +1100

    Update __init__.py
    Co-Authored-By: MingyanYuu <71819498+MingyanYuu@users.noreply.github.com>
    Co-Authored-By: ZiweiLi9908 <71751001+ZiweiLi9908@users.noreply.github.com>

--- Project/FilmFinder/admin/__init__.py
+++ Project/FilmFinder/admin/__init__.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time   : 2020/10/1 12:45
+
+
 # define blueprint of manage function
 from flask import Blueprint
 admin = Blueprint("admin", "admin", url_prefix="/admin")
 
 from FilmFinder.admin import views
+
