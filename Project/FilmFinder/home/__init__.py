141b28b2ef710e7571e7cf90043977671bcde463
UMRKawhi <48422665+UMRKawhi@users.noreply.github.com>
2020-11-14 19:22:15 +1100

    Delete all files

--- Project/FilmFinder/home/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-from FilmFinder.home import views
-
-home = Blueprint("home", __name__)
--- Project/app.py
+++
@@ -1,5 +0,0 @@
-from FilmFinder import app, db
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
